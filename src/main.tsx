35477849cea0698e910d801188723d50635a711c
Guillem Blasco <gowet92@gmail.com>
2024-08-20 22:24:54 +0200

    Relocate files on entity name folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/App/App";
+import App from "./post/components/App/App";
 import "@fontsource/roboto";
 import "./styles/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
