8f0bb5c09aa62aa6d0c4c69719f713d17de494f5
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-08-21 15:59:50 +0200

    Create NotFoundPage page

--- src/post/pages/PostsListPage/PostsListPage.tsx
+++ src/post/pages/PostsListPage/PostsListPage.tsx
@@ -1,4 +1,4 @@
-import Card from "../components/Card/Card";
+import Card from "../../components/Card/Card";
 
 export const PostsListPage = (): React.ReactElement => {
   return <Card title="The litle spider with 8 legs" />;
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -4,12 +4,14 @@ import {
   Route,
 } from "react-router-dom";
 import App from "../post/components/App/App";
-import PostsListPage from "../post/pages/PostsListPage";
+import PostsListPage from "../post/pages/PostsListPage/PostsListPage";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="posts" element={<PostsListPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Route>,
   ),
 );
