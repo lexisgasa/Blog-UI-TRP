52672e843f0e85ecf4084cce469b2f8c79c149b6
Guillem Blasco <gowet92@gmail.com>
2024-08-22 17:04:21 +0200

    Render PostsList componente  in PostsListPage

--- src/post/pages/PostsListPage/PostsListPage.tsx
+++ src/post/pages/PostsListPage/PostsListPage.tsx
@@ -1,9 +1,11 @@
+import PostsList from "../../components/PostsList/PostsList";
 import "./PostsListPage.css";
 
 export const PostsListPage = (): React.ReactElement => {
   return (
     <>
       <h2 className="post-list">Posts List</h2>
+      <PostsList posts={[]} />
     </>
   );
 };
