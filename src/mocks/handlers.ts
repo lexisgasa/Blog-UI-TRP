ff279b283cfbc2f606e85a4ed7b3d992324146e7
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-08-24 11:48:48 +0200

    Create a use case for the test component PostsListPage

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,23 @@
+import { http, HttpResponse } from "msw";
+import { Post } from "../post/types";
+
+export const handlers = [
+  http.get(`${import.meta.env.VITE_API_URL}posts`, () => {
+    return HttpResponse.json<Post[]>([
+      {
+        id: "1",
+        author: "",
+        title: "el leon de la carcel",
+        content: "",
+        date: new Date(),
+      },
+      {
+        id: "2",
+        author: "",
+        title: "un elefante en la luna",
+        content: "",
+        date: new Date(),
+      },
+    ]);
+  }),
+];
--- src/post/pages/PostsListPage/PostsListPage.test.tsx
+++ src/post/pages/PostsListPage/PostsListPage.test.tsx
@@ -13,4 +13,20 @@ describe("Given the PostsListPage component", () => {
       expect(postsListText).toBeInTheDocument();
     });
   });
+
+  describe("When it renders 2 posts with the title 'el leon de la carcel' and 'un elefante en la luna'", () => {
+    test("Then it should show 'el leon de la carcel' and 'un elefante en la luna' inside their headings", async () => {
+      render(<PostsListPage />);
+
+      const lionPostTitle = await screen.findByRole("heading", {
+        name: "el leon de la carcel",
+      });
+      const elephantPostTitle = await screen.findByRole("heading", {
+        name: "un elefante en la luna",
+      });
+
+      expect(lionPostTitle).toBeInTheDocument();
+      expect(elephantPostTitle).toBeInTheDocument();
+    });
+  });
 });
