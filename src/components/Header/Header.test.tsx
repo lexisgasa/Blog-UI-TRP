2546b92198596af5b8519497117d1fb201f02991
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-08-29 10:05:58 +0200

    Refactor Header and NavMenu tests

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -5,13 +5,16 @@ import { MemoryRouter } from "react-router-dom";
 describe("Given the Header component", () => {
   describe("When it is rendered", () => {
     test("Then it should show BLOG-TRP inside a heading", () => {
+      const appTitleText = "blog-trp";
+      const appTitleTextRegex = new RegExp(appTitleText, "i");
+
       render(
         <MemoryRouter>
           <Header />
         </MemoryRouter>,
       );
 
-      const appTitle = screen.getByRole("heading", { name: /blog-trp/i });
+      const appTitle = screen.getByRole("heading", { name: appTitleTextRegex });
 
       expect(appTitle).toBeInTheDocument();
     });
--- src/components/NavMenu/NavMenu.test.tsx
+++ src/components/NavMenu/NavMenu.test.tsx
@@ -5,15 +5,20 @@ import { MemoryRouter } from "react-router-dom";
 describe("Given the NavMenu component", () => {
   describe("When it is rendered", () => {
     test("Then it should show 2 links with the text 'Posts' and 'Create post'", () => {
+      const postsLinkText = "posts";
+      const postsLinkRegex = new RegExp(postsLinkText, "i");
+      const createPostsLinkText = "create post";
+      const createPostsLinkTextRegex = new RegExp(createPostsLinkText, "i");
+
       render(
         <MemoryRouter>
           <NavMenu />
         </MemoryRouter>,
       );
 
-      const postsLink = screen.getByRole("link", { name: /posts/i });
+      const postsLink = screen.getByRole("link", { name: postsLinkRegex });
       const createPostsLink = screen.getByRole("link", {
-        name: /create post/i,
+        name: createPostsLinkTextRegex,
       });
 
       expect(postsLink).toBeInTheDocument();
