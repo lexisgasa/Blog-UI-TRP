7845603ee295bed6cf0dec10bcfd13a482f644c6
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-08-29 09:41:20 +0200

    Add MemoryRouter in Header and NavMenu tests components

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import Header from "./Header";
+import { MemoryRouter } from "react-router-dom";
 
 describe("Given the Header component", () => {
   describe("When it is rendered", () => {
     test("Then it should show BLOG-TRP inside a heading", () => {
-      render(<Header />);
+      render(
+        <MemoryRouter>
+          <Header />
+        </MemoryRouter>,
+      );
 
       const appTitle = screen.getByRole("heading", { name: /blog-trp/i });
 
--- src/components/NavMenu/NavMenu.test.tsx
+++ src/components/NavMenu/NavMenu.test.tsx
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import NavMenu from "./NavMenu";
+import { MemoryRouter } from "react-router-dom";
 
 describe("Given the NavMenu component", () => {
   describe("When it is rendered", () => {
     test("Then it should show 2 links with the text 'Posts' and 'Create post'", () => {
-      render(<NavMenu />);
+      render(
+        <MemoryRouter>
+          <NavMenu />
+        </MemoryRouter>,
+      );
 
       const postsLink = screen.getByRole("link", { name: /posts/i });
       const createPostsLink = screen.getByRole("link", {
