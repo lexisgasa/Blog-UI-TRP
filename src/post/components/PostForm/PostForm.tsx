d6f763f2248bc14df1d7a8940d0b3155140c0369
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-08-29 18:15:21 +0200

    Handle state of each form field

--- src/post/components/PostForm/PostForm.tsx
+++ src/post/components/PostForm/PostForm.tsx
@@ -1,52 +1,87 @@
+import { useState } from "react";
 import "./PostForm.css";
+import { PostData } from "../../types";
 
 const PostForm = (): React.ReactElement => {
+  const [postData, setPostData] = useState<Omit<PostData, "date">>({
+    author: "",
+    title: "",
+    content: "",
+    imageUrl: "",
+    alternativeText: "",
+  });
+
+  const handlePostData = (
+    event:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>,
+  ): void => {
+    setPostData((currentPostData) => ({
+      ...currentPostData,
+      [event.target.id]: event.target.value,
+    }));
+  };
+
   return (
-    <form action="POST" className="post-form">
-      <div className="form-author">
-        <label htmlFor="form-author">Author:</label>
-        <input
-          type="text"
-          id="form-author"
-          placeholder="Insert your name"
-          required
-        />
-      </div>
-      <div className="form-title">
-        <label htmlFor="form-title">Title:</label>
-        <input id="form-title" placeholder="Insert post title" required />
-      </div>
-      <div className="form-imageUrl">
-        <label htmlFor="form-imageUrl">Image URL:</label>
-        <input
-          id="form-imageUrl"
-          placeholder="Insert post image URL"
-          required
-        />
-      </div>
-      <div className="form-imageAlternativeText">
-        <label htmlFor="form-imageAlternativeText">
-          Image alternative text:{" "}
-        </label>
-        <input
-          id="form-imageAlternativeText"
-          placeholder="Insert post image alternative text"
-          required
-        />
-      </div>
-      <div className="form-content">
-        <label htmlFor="form-content">Content:</label>
-        <textarea
-          id="form-content"
-          placeholder="Insert post content"
-          className="form-content__text"
-          required
-        />
-      </div>
-      <button type="submit" className="post-form__button">
-        Send
-      </button>
-    </form>
+    <>
+      <form className="post-form">
+        <div className="post-form__form-group">
+          <label htmlFor="author">Author:</label>
+          <input
+            type="text"
+            id="author"
+            placeholder="Insert your name"
+            value={postData.author}
+            onChange={handlePostData}
+            required
+          />
+        </div>
+        <div className="post-form__form-group">
+          <label htmlFor="title">Title:</label>
+          <input
+            id="title"
+            placeholder="Insert post title"
+            value={postData.title}
+            onChange={handlePostData}
+            required
+          />
+        </div>
+        <div className="post-form__form-group">
+          <label htmlFor="imageUrl">Image URL:</label>
+          <input
+            id="imageUrl"
+            placeholder="Insert post image URL"
+            value={postData.imageUrl}
+            onChange={handlePostData}
+            required
+          />
+        </div>
+        <div className="post-form__form-group">
+          <label htmlFor="alternativeText">Image alternative text:</label>
+          <input
+            id="alternativeText"
+            placeholder="Insert post image alternative text"
+            value={postData.alternativeText}
+            onChange={handlePostData}
+            required
+          />
+        </div>
+        <div className="post-form__form-group">
+          <label htmlFor="content">Content:</label>
+          <textarea
+            id="content"
+            placeholder="Insert post content"
+            className="form-content__text"
+            value={postData.content}
+            onChange={handlePostData}
+            required
+          />
+        </div>
+        <button type="submit" className="post-form__button">
+          Send
+        </button>
+      </form>
+    </>
   );
 };
 
