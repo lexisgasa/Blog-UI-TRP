8b520f33407ac301dacca8620a2ff49a9f031d14
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-08-31 11:37:07 +0200

    Change mock structure as it now receives a different datatype also

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,25 +3,27 @@ import { Post } from "../post/types";
 
 export const handlers = [
   http.get(`${import.meta.env.VITE_API_URL}posts`, () => {
-    return HttpResponse.json<Post[]>([
-      {
-        id: "1",
-        author: "",
-        title: "el leon de la carcel",
-        content: "",
-        date: new Date(),
-        alternativeText: "",
-        imageUrl: "",
-      },
-      {
-        id: "2",
-        author: "",
-        title: "un elefante en la luna",
-        content: "",
-        date: new Date(),
-        alternativeText: "",
-        imageUrl: "",
-      },
-    ]);
+    return HttpResponse.json<{ posts: Post[] }>({
+      posts: [
+        {
+          id: "1",
+          author: "",
+          title: "el leon de la carcel",
+          content: "",
+          date: new Date(),
+          alternativeText: "",
+          imageUrl: "",
+        },
+        {
+          id: "2",
+          author: "",
+          title: "un elefante en la luna",
+          content: "",
+          date: new Date(),
+          alternativeText: "",
+          imageUrl: "",
+        },
+      ],
+    });
   }),
 ];
